ba984139c1215bd6e1dc084411cffdad9dde3533
Tomoya Koyanagi <tomk79@gmail.com>
2020-05-23 12:19:41 +0900

    update gulp and libs

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,27 @@
-var gulp = require('gulp');
-var webpack = require('webpack');
-var webpackStream = require('webpack-stream');
-var sass = require('gulp-sass');//CSSコンパイラ
-var autoprefixer = require("gulp-autoprefixer");//CSSにベンダープレフィックスを付与してくれる
-var minifyCss = require('gulp-minify-css');//CSSファイルの圧縮ツール
-var uglify = require("gulp-uglify");//JavaScriptファイルの圧縮ツール
-var concat = require('gulp-concat');//ファイルの結合ツール
-var plumber = require("gulp-plumber");//コンパイルエラーが起きても watch を抜けないようになる
-var rename = require("gulp-rename");//ファイル名の置き換えを行う
-var packageJson = require(__dirname+'/package.json');
-var _tasks = [
-	'client-libs',
-	'gitui79.js',
-	'.css.scss'
-];
+let gulp = require('gulp');
+let webpack = require('webpack');
+let webpackStream = require('webpack-stream');
+let sass = require('gulp-sass');//CSSコンパイラ
+let autoprefixer = require("gulp-autoprefixer");//CSSにベンダープレフィックスを付与してくれる
+let minifyCss = require('gulp-minify-css');//CSSファイルの圧縮ツール
+let uglify = require("gulp-uglify");//JavaScriptファイルの圧縮ツール
+let concat = require('gulp-concat');//ファイルの結合ツール
+let plumber = require("gulp-plumber");//コンパイルエラーが起きても watch を抜けないようになる
+let rename = require("gulp-rename");//ファイル名の置き換えを行う
+let packageJson = require(__dirname+'/package.json');
+
 
 // client-libs (frontend) を処理
-gulp.task("client-libs", function() {
-	// gulp.src(["node_modules/px2style/dist/**/*"])
+gulp.task("client-libs", function(callback) {
+	// return gulp.src(["node_modules/px2style/dist/**/*"])
 	// 	.pipe(gulp.dest( './tests/app/client/libs/px2style/dist/' ))
 	// ;
+	callback();
 });
 
 // src 中の *.css.scss を処理
 gulp.task('.css.scss', function(){
-	gulp.src("src/**/*.css.scss")
+	return gulp.src("src/**/*.css.scss")
 		.pipe(plumber())
 		.pipe(sass({
 			"sourceComments": false
@@ -71,14 +68,23 @@ gulp.task("gitui79.js", function() {
 	;
 });
 
-// src 中のすべての拡張子を監視して処理
-gulp.task("watch", function() {
-	gulp.watch(["src/**/*"], _tasks);
+// ブラウザを立ち上げてプレビューする
+gulp.task("preview", function(callback) {
+	require('child_process').spawn('open',['http://127.0.0.1:3000/']);
+	callback();
 });
 
-// ブラウザを立ち上げてプレビューする
-gulp.task("preview", function() {
-	require('child_process').spawn('open',['http://127.0.0.1:3000/']);
+
+
+let _tasks = gulp.parallel(
+	'client-libs',
+	'gitui79.js',
+	'.css.scss'
+);
+
+// src 中のすべての拡張子を監視して処理
+gulp.task("watch", function() {
+	return gulp.watch(["src/**/*"], _tasks);
 });
 
 // src 中のすべての拡張子を処理(default)
